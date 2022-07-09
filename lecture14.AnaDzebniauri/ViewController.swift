00fbab18cfa24c42425305e3e77265d15276736a
anadzebniauri <anadzebniauri13@gmail.com>
2022-07-09 16:50:34 +0400

    Initial Commit

---
+++ lecture14.AnaDzebniauri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lecture14.AnaDzebniauri
+//
+//  Created by Ana Dzebniauri on 09.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
